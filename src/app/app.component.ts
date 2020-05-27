29fee8570953dd206644bc571dcac2031929db3b
Rishi Kumar <rishikumar15@gmail.com>
2020-05-27 09:25:38 +0530

    1st Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators,FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,109 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveForm';
+  countryList = [];
+  stateList = [];
+  cityList = [];
+  profileForm: FormGroup;
+  constructor() {
+    this.profileForm = new FormGroup({
+      food: new FormControl('', [Validators.required,Validators.pattern('[a-zA-Z]{4,}'),]),
+      color: new FormControl('', [Validators.required,Validators.pattern('[a-zA-Z]{4,}'),]),
+      name: new FormControl('', [Validators.required,Validators.pattern('[a-zA-Z]{4,}'),]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required,Validators.pattern('[0-9+-]{10}'),]),
+      pass: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmpass: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
+    });
+    this.profileForm.get('country').valueChanges.subscribe((val) => {
+      this.updateStateList(val);
+      this.cityList = [];
+    });
+    this.profileForm.get('state').valueChanges.subscribe((val) => {
+      this.updateCityList(val);
+    });
+    this.countryList = this.data;
+  }
+
+  updatePatt() {
+    this.profileForm
+      .get('confirmpass')
+      .setValidators([
+        Validators.required,
+        Validators.pattern(this.profileForm.get('pass').value),
+      ]);
+    this.profileForm
+      .get('confirmpass')
+      .updateValueAndValidity({ onlySelf: true, emitEvent: true });
+  }
+  consoleDetails() {
+    console.log(this.profileForm.value);
+  }
+
+  updateStateList(countryCode) {
+    this.countryList.forEach((country) => {
+      if (country.code == countryCode) {
+        this.stateList = country.states;
+      }
+    });
+    this.profileForm.get('state').setValue('');
+  }
+
+  updateCityList(stateCode) {
+    this.stateList.forEach((state) => {
+      if (state.code == stateCode) {
+        this.cityList = state.cities;
+      }
+    });
+    this.profileForm.get('city').setValue('');
+  }
+
+  data = [
+    {
+      name: 'India',
+      code: 'IND',
+      states: [
+        {
+          name: 'TamilNadu',
+          code: 'TN',
+          cities: [
+            { name: 'Chennai', code: 'CHN' },
+            { name: 'Madurai', code: 'MAD' },
+          ],
+        },
+        {
+          name: 'Karnataka',
+          code: 'KA',
+          cities: [
+            { name: 'Mysore', code: 'MY' },
+            { name: 'Bangalore', code: 'BA' },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'United States of America',
+      code: 'USA',
+      states: [
+        {
+          name: 'New York State',
+          code: 'NY',
+          cities: [
+            { name: 'New York city', code: 'NYC' },
+            { name: 'Buffalo', code: 'BU' },
+          ],
+        },
+        {
+          name: 'Los Santos',
+          code: 'LS',
+          cities: [
+            { name: '‎Mount Chillad', code: 'MC' },
+            { name: 'Blaine County', code: 'BC' },
+          ],
+        },
+      ],
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
